48a5eb5f80cdebb21c5f57381988040c4381deab
Judyk247 <judechukwunalua@gmail.com>
2025-09-26 08:16:41 +0100

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,7 @@
-# Trading settings
+# Trading settings - Dynamic for PocketOption
 TRADING_SETTINGS = {
-    'timeframes': ['1m', '2m', '3m', '5m'],
-    'assets': [
-        'EURUSD', 'EURUSD_otc', 'GBPUSD', 'GBPUSD_otc', 
-        'USDJPY', 'USDJPY_otc', 'AUDUSD', 'AUDUSD_otc',
-        'USDCAD', 'USDCAD_otc', 'USDCHF', 'USDCHF_otc',
-        'BTCUSD_otc', 'ETHUSD_otc', 'XAUUSD', 'XAUUSD_otc'
-    ],
+    'timeframes': ['1m', '2m', '3m', '5m'],  # PocketOption common timeframes
+    'assets': [],  # Will be populated dynamically from PocketOption API
     'max_concurrent_trades': 3,
     'risk_per_trade': 2.0,  # 2% of account per trade
     'stop_loss_pips': 20,
@@ -26,3 +21,36 @@ STRATEGY_SETTINGS = {
         'min_confidence': 65
     }
 }
+
+# PocketOption specific settings
+POCKET_OPTION_CONFIG = {
+    'api_endpoints': {
+        'instruments': 'https://pocketoption.com/api/instruments',  # Endpoint to get available assets
+        'categories': 'https://pocketoption.com/api/categories'     # Endpoint to get asset categories
+    },
+    'preferred_categories': ['forex', 'crypto', 'indices', 'stocks'],  # Priority order for asset types
+    'trading_hours': {
+        'forex': '24/5',
+        'crypto': '24/7',
+        'indices': 'market_hours',
+        'stocks': 'market_hours'
+    }
+}
+
+def get_default_assets():
+    """Return a basic set of popular assets if dynamic loading fails"""
+    return [
+        'EURUSD', 'GBPUSD', 'USDJPY', 'AUDUSD', 
+        'USDCAD', 'USDCHF', 'XAUUSD', 'XAGUSD',
+        'BTC', 'ETH', 'LTC', 'XRP',
+        'US500', 'US30', 'NAS100', 'JP225'
+    ]
+
+async def load_available_assets():
+    """
+    Load available trading instruments from PocketOption API
+    This should be called after successful authentication
+    """
+    # This function will be implemented in the WebSocket client
+    # after we receive the instruments list from PocketOption
+    pass
